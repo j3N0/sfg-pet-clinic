004ccd1729b71458542c6e86fadfe0c442400504
J3N0 <3223529700@qq.com>
2019-06-16 22:11:33 +0800

    Implement Map Impl for Visit Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/springdatajpa/VisitService.java
@@ -0,0 +1,7 @@
+package com.example.sfgpetclinic.services.springdatajpa;
+
+import com.example.sfgpetclinic.model.Visit;
+import com.example.sfgpetclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
