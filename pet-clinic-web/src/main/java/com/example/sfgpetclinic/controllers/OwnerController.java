120121a5f33dd6feb19219c385e26c5fb1882f47
J3N0 <3223529700@qq.com>
2019-06-19 16:57:46 +0800

    Display Owner Details.

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.example.sfgpetclinic.controllers;
 import com.example.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -27,5 +30,12 @@ public class OwnerController {
     public String findOwners() {
 
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
